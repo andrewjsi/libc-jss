0648a4284888773607657f22824f23d1b020f998
Andras Jeszenszky <jsi@jss.hu>
2012-06-15 06:33:21 +0200

    debug.h: makro parameter zarojelekkel levedve

--- debug.h
+++ debug.h
@@ -1,6 +1,6 @@
 // JSS DEBUG
 
-#define debi(x) printf(#x " = %d\n", x)
-#define debf(x) printf(#x " = %f\n", x)
-#define debs(x) printf(#x " = %s\n", x)
+#define debi(x) printf(#x " = %d\n", (x))
+#define debf(x) printf(#x " = %f\n", (x))
+#define debs(x) printf(#x " = %s\n", (x))
 
