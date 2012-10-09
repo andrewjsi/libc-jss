8f3db3aa81d0856f52900c8fe26be10226f525a3
Andras Jeszenszky <jsi@jss.hu>
2012-10-10 01:58:42 +0200

    korrektebb tipuskenyszerites a makro parametereket figyelembe veve

--- debug.h
+++ debug.h
@@ -1,6 +1,6 @@
 // JSS DEBUG
 
-#define debi(x) printf(#x " = %d\n", ((int)x))
+#define debi(x) printf(#x " = %d\n", ((int)(x)))
 #define debf(x) printf(#x " = %f\n", (x))
 #define debs(x) printf(#x " = %s\n", (x))
 
