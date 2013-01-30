58b4917ef4e00270f5570d300fd75d94ef985fa7
Andras Jeszenszky <jsi@jss.hu>
2013-01-30 19:49:20 +0100

    misc.c: retab

--- misc.c
+++ misc.c
@@ -4,11 +4,11 @@
 #include <string.h>
 
 char *chomp (char *str) {
-	if (str == NULL)
-		return NULL;
-	size_t len = strlen(str);
-	if (str[len - 1] == '\n')
-		str[len - 1] = '\0';
-	return str;
+    if (str == NULL)
+        return NULL;
+    size_t len = strlen(str);
+    if (str[len - 1] == '\n')
+        str[len - 1] = '\0';
+    return str;
 }
 
