cb5e70a3eef53829d64ef62719f3bf5c411ef0ea
JSI <jsi@jss.hu>
2014-06-03 22:38:33 +0200

    misc.c: die() bevezetve

--- misc.c
+++ misc.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <arpa/inet.h>  // inet_pton() függvény
+#include <stdarg.h>
 
 #include "debug.h"
 
@@ -207,4 +208,12 @@ int is_valid_ip (const char *ip) {
     struct sockaddr_in sa;
     int result = inet_pton(AF_INET, ip, &(sa.sin_addr));
     return result != 0;
+}
+
+void die (const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    exit(-1);
 }
